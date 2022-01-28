14e322772467f3dc94265b2f9dc341a9d3f07219
nadis-test <nadis.letters@gmail.com>
2022-01-28 19:16:57 +0300

    task 1-2

--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -10,4 +10,11 @@ public class MainClassTest extends MainClass{
 
         Assert.assertTrue("expected result " + expected + " != actual result " + getLocalNumber(), expected == getLocalNumber());
     }
+
+    @Test
+    public void testGetClasslNumber() {
+        int expected = 45;
+
+        Assert.assertTrue("Class number value" + getClassNumber() + " <= expected value " + expected, getClassNumber() > expected);
+    }
 }
